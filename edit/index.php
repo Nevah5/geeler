d66251cc6c3f1f80b09ce68c8914290e28da0d85
Nevah5 <neevaah5@gmail.com>
2021-12-26 14:49:53 +0100

    added design to edit page

--- edit/index.php
+++ edit/index.php
@@ -13,6 +13,7 @@ $lang = $_GET["lang"];
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>Edit home - geeler.net</title>
 </head>
 <body>
@@ -29,16 +30,20 @@ $lang = $_GET["lang"];
       $types = mysqli_query($con, "SELECT DISTINCT type FROM lang WHERE lang='$lang'");
       foreach($types as $value){
         $type = $value["type"];
-        echo "<h1>$type</h1>";
+        echo "<div>";
+        echo "<h1>".strtoupper($type)."</h1>";
         $sitedata = mysqli_query($con, "SELECT * FROM lang WHERE lang='$lang' AND type='$type'");
-        foreach($sitedata as $key => $value){
+        foreach($sitedata as $value){
           echo "
-          <form action=\"./?lang=".$lang ."&id=".$key."\" method=\"POST\">
-            <textarea name=\"site\" id=\"site\" cols=\"30\" rows=\"10\">".htmlspecialchars($value['content'])."</textarea>
-            <input type=\"submit\">
-          </form>
+            <form action=\"./?lang=".$lang ."&id=".$value['ID']."\" method=\"POST\">
+              <h3>".$value['title']."</h3>
+              <textarea name=\"site\" id=\"site\" cols=\"30\" rows=\"10\">".htmlspecialchars($value['content'])."</textarea>
+              <label for=\"submitbtn".$value['ID']."\" id=\"submitbtn\">Update</label>
+              <input type=\"submit\" id=\"submitbtn".$value['ID']."\">
+            </form>
           ";
         }
+        echo "</div>";
       }
       // print_r($sitedata);
     }
