1f5262769ff3056bb6affb61b0a3ecb17f343128
Nevah5 <neevaah5@gmail.com>
2021-12-26 13:31:20 +0100

    undo change in compile.php, accidently deleted compiler script

--- compiler/compile.php
+++ compiler/compile.php
@@ -1,66 +1,49 @@
 <?php
-  session_start();
-  if(!$_SESSION["login"] || $_SESSION["username"] != "admin"){
-    header("Location: ../");
+session_start();
+$con = mysqli_connect("ubibudud.mysql.db.internal", "ubibudud_geeler", 'qucoCr=$Es=uzaWret5I', "ubibudud_geeler");
+
+if(!isset($_GET["input"]) || !isset($_GET["lang"]) || !isset($_GET["output"])){
+  header("Location: ../");
+}
+if(!$_SESSION["login"] && $_SESSION["username"] == "admin"){
+  header("Location: ../");
+}
+
+$input = "../media/raw/" . $_GET["input"]; //input file with placeholders
+$lang = strtoupper($_GET["lang"]);
+$output = "../" . $_GET["output"]; //output
+
+$replace = [];
+//load contents to replace into array from database
+$data = mysqli_query($con, "SELECT * FROM lang WHERE lang='$lang'");
+foreach($data as $value){
+  $replace[$value["type"] . "." . $value["title"]] = $value["content"];
+}
+
+//replace placeholders
+$handle = fopen($input, "r");
+$new_content = "";
+if ($handle) {
+  while (($line = fgets($handle)) != false) {
+    if (preg_match('/[${]{1}.+[}]{1}/', $line)) {
+      $index = explode('${', explode('}', $line)[0])[1]; //gets ${this}
+      $replace_with = $replace[$index]; //gets contents to replace with placeholder
+      $line = preg_replace('/[${]{1}.+[}]{1}/', $replace_with, $line); //replaces placeholder
+      $new_content .= $line;
+    } else {
+      $new_content .= $line;
+    }
   }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Compile Sites</title>
-</head>
-<body>
-  <div>
-    <div>
-      <a href="/">HOME</a>
-      <a href='compile.php?input=home.php&lang=en&output=index.php'>Home Index [EN]</a>
-      <a href='compile.php?input=home.php&lang=de&output=index.php'>Home Index [DE]</a>
-    </div>
-    <div>
-      <a href="/login/">LOGIN</a>
-      <a href='compile.php?input=login.php&lang=en&output=login/index.php'>Login Index [EN]</a>
-      <a href='compile.php?input=login.php&lang=de&output=login/index.php'>Login Index [DE]</a>
-    </div>
-    <div>
-      <a href="/register/">REGISTER</a>
-      <a href='compile.php?input=register.php&lang=en&output=register/index.php'>Register Index [EN]</a>
-      <a href='compile.php?input=register.php&lang=de&output=register/index.php'>Register Index [DE]</a>
-    </div>
-  </div>
-</body>
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Alata&family=Concert+One&family=Russo+One&display=swap');
-  body{
-    margin: 0;
-    padding: 0;
-    height: 100vh;
-  }
-  body > div{
-    height: 100%;
-    width: 100%;
-  }
-  div{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 30px;
-  }
-  div > div{
-    flex-direction: column;
-    gap: 10px;
-  }
-  a{
-    display: block;
-    width: fit-content;
-    padding: 5px 10px;
-    color: white;
-    background-color: #144A8F;
-    border-radius: .5em;
-    text-decoration: none;
-    font-family: Alata;
-  }
-</style>
-</html>
+  fclose($handle);
+} else {
+  echo "Error opening \"$file_replace\"";
+  exit;
+}
+
+// save compiled file
+file_put_contents($output, $new_content);
+
+// $redirect = explode("index.php", $output)[0];
+// print_r($replace);
+mysqli_close($con);
+header("Location: index.php");
