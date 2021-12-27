1d18a9a50b088d17375220b678db59c33a433daa
Nevah5 <neevaah5@gmail.com>
2021-12-27 13:53:08 +0100

    changed footer placeholders and added 404 page with style

--- media/raw/404.php
+++ media/raw/404.php
@@ -27,34 +27,34 @@
   <a href="/" class="logo"><span>geeler</span></a>
   <div class="error"><div class="content">
     <span>404</span>
-    <p>Did you mean <?= "<a href=\"$closest\">geeler.net".$closest."</a>?" ?></p>
+    <p>${404.didyoumean} <?= "<a href=\"$closest\">geeler.net".$closest."</a>?" ?></p>
   </div></div>
   <div class="footer">
     <nav>
       <div>
-        <h1>Home</h1>
-        <a href="#general">About me</a>
-        <a href="#hobby">My hobby</a>
-        <a href="#motivation">Motivation</a>
-        <a href="#whyme">Why me?</a>
+        <h1>${home.navbar.home}</h1>
+        <a href="#general">${home.about.title}</a>
+        <a href="#hobby">${home.about.hobby.title}</a>
+        <a href="#motivation">${home.about.motivation.title}</a>
+        <a href="#whyme">${home.about.whyme.title}</a>
       </div>
       <div>
-        <h1>Contact</h1>
-        <a href="tel:+41789500085">Phone</a>
-        <a href="email:contact@geeler.net">Email</a>
+        <h1>${home.navbar.contact}</h1>
+        <a href="tel:+41789500085">${home.contact.phone}</a>
+        <a href="email:contact@geeler.net">${home.contact.email}</a>
       </div>
       <div>
-        <h1>Donate</h1>
+        <h1>${home.navbar.donate}</h1>
         <a href="https://www.paypal.com/donate/?hosted_button_id=AP8EUCER58QRA" target="_blank">PayPal</a>
         <a href="https://www.buymeacoffee.com/nevah5" target="_blank">Buy me a coffee</a>
       </div>
       <div>
-        <h1>Stuff</h1>
-        <a href="/tos/">Terms of service</a>
-        <a href="/privacy">Privacy</a>
-        <a href="/guidelines/">Guidelines</a>
-        <a href="/acknownledgements/">Acknownledgements</a>
-        <a href="/copyright/">Licence</a>
+        <h1>${footer.stuff.title}</h1>
+        <a href="/tos/">${footer.stuff.tos}</a>
+        <a href="/privacy">${footer.stuff.privacy}</a>
+        <a href="/guidelines/">${footer.stuff.guidelines}</a>
+        <a href="/acknownledgements/">${footer.stuff.acknownledgements}</a>
+        <a href="/copyright/">${footer.stuff.licence}</a>
       </div>
     </nav>
     <div class="wrapper">
@@ -71,7 +71,7 @@
         <?php
           if(!$_SESSION["login"]){
         ?>
-        <a href="/login/" class="login">Login</a>
+        <a href="/login/" class="login">${home.user.login}</a>
         <?php
           }else{
         ?>
--- media/raw/login.php
+++ media/raw/login.php
@@ -94,10 +94,10 @@
     <nav>
       <div>
         <h1>${home.navbar.home}</h1>
-        <a href="#general">About Me</a>
-        <a href="#hobby">My hobby</a>
-        <a href="#motivation">Motivation</a>
-        <a href="#whyme">Why me?</a>
+        <a href="#general">${home.about.title}</a>
+        <a href="#hobby">${home.about.hobby.title}</a>
+        <a href="#motivation">${home.about.motivation.title}</a>
+        <a href="#whyme">${home.about.whyme.title}</a>
       </div>
       <div>
         <h1>${home.navbar.contact}</h1>
