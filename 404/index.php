9754c194f3b0c170efd806dd4ecadceaefd9dcd2
Nevah5 <neevaah5@gmail.com>
2021-12-27 13:22:44 +0100

    added footer to 404 page

--- 404/index.php
+++ 404/index.php
@@ -11,8 +11,8 @@
   <title>404 - geeler.net</title>
 </head>
 <body>
-  <a href="/" class="logo">geeler</a>
-  <div>404</div>
+  <a href="/" class="logo"><span>geeler</span></a>
+  <div id="error">404</div>
   <?php
     $sites = ["/home/", "/login/", "/register/"];
     $shortest = 100;
@@ -27,5 +27,61 @@
     }
     echo "<p>Did you mean: " . "<a href=\"$closest\">geeler.net".$closest."</a>?</p>" . PHP_EOL;
   ?>
+  <div class="footer">
+    <nav>
+      <div>
+        <h1>${home.navbar.home}</h1>
+        <a href="#general">About Me</a>
+        <a href="#hobby">My hobby</a>
+        <a href="#motivation">Motivation</a>
+        <a href="#whyme">Why me?</a>
+      </div>
+      <div>
+        <h1>${home.navbar.contact}</h1>
+        <a href="tel:+41789500085">${home.contact.phone}</a>
+        <a href="email:contact@geeler.net">${home.contact.email}</a>
+      </div>
+      <div>
+        <h1>${home.navbar.donate}</h1>
+        <a href="https://www.paypal.com/donate/?hosted_button_id=AP8EUCER58QRA" target="_blank">PayPal</a>
+        <a href="https://www.buymeacoffee.com/nevah5" target="_blank">Buy me a coffee</a>
+      </div>
+      <div>
+        <h1>${footer.stuff.title}</h1>
+        <a href="/tos/">${footer.stuff.tos}</a>
+        <a href="/privacy">${footer.stuff.privacy}</a>
+        <a href="/guidelines/">${footer.stuff.guidelines}</a>
+        <a href="/acknownledgements/">${footer.stuff.acknownledgements}</a>
+        <a href="/copyright/">${footer.stuff.licence}</a>
+      </div>
+    </nav>
+    <div class="wrapper">
+      <a href="https://github.com/Nevah5" target="_blank"></a>
+      <a href="https://reddit.com/u/Nevah5" target="_blank"></a>
+      <a href="https://steamcommunity.com/id/nevah5/" target="_blank"></a>
+      <a href="https://twitter.com/Neevaah5" target="_blank"></a>
+      <a href="https://stackoverflow.com/users/16029189/nevah5" target="_blank"></a>
+    </div>
+    <div class="foot">
+      <div class="spacer"></div>
+      <div class="row">
+        <h1>geeler</h1>
+        <?php
+          if(!$_SESSION["login"]){
+        ?>
+        <a href="/login/" class="login">${home.user.login}</a>
+        <?php
+          }else{
+        ?>
+        <a class="user" href="/logout/">
+          <div class="image" style="background-image: url('/media/icons/user.png');"></div>
+          <span class="account"><?= $_SESSION["username"] ?></span>
+        </a>
+        <?php
+          }
+        ?>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
