359da88f0433604520a1a4deb0768ab6a2fc4cba
Nevah5 <neevaah5@gmail.com>
2022-01-08 18:16:38 +0100

    Updated lang into sql converter

--- media/lang/index.php
+++ media/lang/index.php
@@ -1,28 +1,30 @@
 <?php
-$lang = "de";
-$file = fopen("$lang.txt", "r");
-$before = false;
-echo "INSERT INTO lang (lang, type, title, content) VALUES" . PHP_EOL;
-while($line = fgets($file)){
-  //split up line with both title and value
-  $content = explode(":", $line);
-  $content = str_replace(array("\r", "\n"), '', $content);
-  $title = $content[0];
-  $value = str_replace('"', '""', $content[1]);
+$langs = ["de", "en"];
+foreach($langs as $lang){
+  $file = fopen("$lang.txt", "r");
+  $before = false;
+  echo "INSERT INTO lang (lang, type, title, content) VALUES" . PHP_EOL;
+  while($line = fgets($file)){
+    //split up line with both title and value
+    $content = explode(":", $line);
+    $content = str_replace(array("\r", "\n"), '', $content);
+    $title = $content[0];
+    $value = str_replace('"', '""', $content[1]);
 
-  //splits title into new and type of tupel
-  $title = explode(".", $title);
-  $type = $title[0];
-  unset($title[0]);
-  $title = implode(".", $title);
+    //splits title into new and type of tupel
+    $title = explode(".", $title);
+    $type = $title[0];
+    unset($title[0]);
+    $title = implode(".", $title);
 
-  //prints the tupel
-  if($title != ""){
-    echo $before ? "," . PHP_EOL : "";
-    $before = true;
-    echo "(\"" . strtoupper($lang) . "\", \"" . $type . "\", \"". $title ."\", \"" . $value . "\")";
+    if($title != ""){
+      echo $before ? "," . PHP_EOL : "";
+      $before = true;
+      echo "(\"" . strtoupper($lang) . "\", \"" . $type . "\", \"". $title ."\", \"" . $value . "\")";
+    }
   }
-}
-echo ";";
+  echo ";";
 
-fclose($file);
+  fclose($file);
+  echo "\n\n";
+}
