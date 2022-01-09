d0d5c4e8a91ddd7162c73aeac524c4c09a03f54e
Nevah5 <neevaah5@gmail.com>
2022-01-09 14:05:55 +0100

    Added resend code 2fa #8

---
+++ login/2FA/resend/index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+$con = mysqli_connect("ubibudud.mysql.db.internal", "ubibudud_geeler", 'qucoCr=$Es=uzaWret5I', "ubibudud_geeler");
+include("../login/autologin.php");
+if(!isset($_SESSION["2FA_sent"])){
+  header("Location: ../../");
+}
+//get latest code send date
+$uID = $_SESSION["2FA_userID"];
+$sql = "SELECT * FROM 2FA WHERE userFK='$uID' AND NOW() + INTERVAL 12 MINUTE < valid ORDER BY valid DESC LIMIT 1";
+$query = mysqli_query($con, $sql);
+//test if code was not last 3 mins
+if(mysqli_num_rows($query) >= 1){
+  $_SESSION["2FA_wait"] = true;
+  header("Location: ../");
+}else{
+  //resend code
+  $_SESSION["2FA"] = true;
+  $_SESSION["2FA_resent"] = true;
+  header("Location: ../../../mailing/");
+}
