c17a936fc0503fa47e84b269eb07e37d58b9fd6f
Nevah5 <neevaah5@gmail.com>
2022-01-09 20:15:02 +0100

    Updated 404 error predicts #19

--- media/raw/404.php
+++ media/raw/404.php
@@ -14,7 +14,7 @@
 </head>
 <body>
   <?php
-    $sites = ["/home/", "/login/", "/register/"];
+    $sites = ["/home/", "/login/", "/register/", "/account/", "/login/2FA/", "/logout/", "/verify/"];
     $shortest = 100;
     $closest = "";
 
