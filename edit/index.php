ea73885a234ed6d74870ae7e6320bc7651802fd1
Nevah5 <neevaah5@gmail.com>
2022-01-15 23:39:13 +0100

    changed paths from media to resources

--- edit/index.php
+++ edit/index.php
@@ -33,7 +33,7 @@ $lang = $_GET["lang"];
     <a href='compile.php?input=login_forgotpassword.php&lang=<?= $lang ?>&output=login/forgotpassword/index.php'>Login/Forgotpassword</a>
     <a href='compile.php?input=register.php&lang=<?= $lang ?>&output=register/index.php'>Register</a>
     <a href='compile.php?input=register_success.php&lang=<?= $lang ?>&output=register/success/index.php'>Register/Success</a>
-    <a href='compile.php?input=footer.php&lang=<?= $lang ?>&output=media/footer/footer.php'>Footer</a>
+    <a href='compile.php?input=footer.php&lang=<?= $lang ?>&output=resources/footer/footer.php'>Footer</a>
     <a href='compile.php?input=401.php&lang=<?= $lang ?>&output=401/index.php'>401</a>
     <a href='compile.php?input=403.php&lang=<?= $lang ?>&output=403/index.php'>403</a>
     <a href='compile.php?input=404.php&lang=<?= $lang ?>&output=404/index.php'>404</a>
