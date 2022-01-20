b018f022bdd27416f7f3302992e9ed191ee16d34
Nevah5 <neevaah5@gmail.com>
2022-01-20 07:45:40 +0100

    Updated stylesheet path in register success page

--- resources/templates/register_success.php
+++ resources/templates/register_success.php
@@ -27,7 +27,7 @@
   <meta name="description" content="${home.meta.desc}">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../login/style.css">
+  <link rel="stylesheet" href="/login/style.css">
   <link rel="icon" href="/resources/icons/logo.png">
   <title>${register_success.title}</title>
 </head>
