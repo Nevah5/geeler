3b3f2f96202b7b2105a18a54b75ec37db85091e6
Nevah5 <neevaah5@gmail.com>
2022-01-22 16:00:56 +0100

    updated link to logout -> now account page #7

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -45,7 +45,7 @@
       <?php
         }else{
       ?>
-      <a class="user" href="/logout/">
+      <a class="user" href="/account/">
         <div class="image" style="background-image: url('/resources/icons/user.png');"></div>
         <span class="account"><?= $_SESSION["username"] ?></span>
       </a>
