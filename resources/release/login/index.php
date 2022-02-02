859b767c0a3770a74f36ab72fc82c4cec2c50e7a
Nevah5 <neevaah5@gmail.com>
2022-02-02 09:22:09 +0100

    Added release site to project

---
+++ resources/release/login/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="robots" content="noindex">
+  <meta name="googlebot" content="noindex">
+  <meta name="googlebot-news" content="noindex">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://geeler.net/login/">
+  <meta property="og:title" content="geeler.net">
+  <meta property="og:description" content="The login page of geeler.net.">
+  <meta property="og:image" content="https://geeler.net/resources/pictures/preview/site.png">
+  <meta name="twitter:card" content="summary">
+  <meta property="twitter:url" content="https://geeler.net/login/">
+  <meta property="twitter:title" content="geeler.net">
+  <meta property="twitter:description" content="The login page of geeler.net.">
+  <meta property="twitter:image" content="https://geeler.net/resources/pictures/preview/site.png">
+  <meta name="author" content="Noah Geeler">
+  <meta name="description" content="The login page of geeler.net.">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <link rel="icon" href="/resources/icons/logo.png">
+  <title>Login - geeler.net</title>
+  <script>
+    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  </script>
+</head>
+<body>
+  <!--
+           __..--''``---....___   _..._    __
+ /// //_.-'    .-/";  `        ``<._  ``.''_ `. / // /
+///_.-' _..--.'_    \                    `( ) ) // //
+/ (_..-' // (< _     ;_..__               ; `' / ///
+ / // // //  `-._,_)' // / ``--...____..-' /// / //
+Site made by Noah Geeler
+  -->
+</body>
+</html>
